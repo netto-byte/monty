ec0a3f305cea259ca921138855e7ecb8ad866172
netto-byte <marvellousmatthew@gmail.com>
2024-02-04 10:55:57 -0800

    Execute commands

---
+++ cmd_execute.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * get_command - Executes a command
+ */
+void get_command(void)
+{
+	instruction_t get[] = {
+		{"push", push}, {"pall", pall}, {NULL, NULL}
+	};
+	int i;
+	char *cmd;
+
+	cmd = strtok(data.command, " \t\n");
+	if (cmd == NULL)
+		return;
+
+	for (i = 0; get[i].opcode; i++)
+	{
+		if ((strcmp(get[i].opcode, cmd)) == 0)
+		{
+			get[i].f(&(data.stack), data.line_number);
+			return;
+		}
+	}
+
+	if (get[i].opcode == NULL)
+	{
+		fprintf(stderr, "L%u: unknown instruction %s\n",
+				data.line_number, cmd);
+		free_data();
+		exit(EXIT_FAILURE);
+	}
+}
