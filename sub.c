0246b0ce1a1003b929b23690c4ae217083624968
netto-byte <marvellousmatthew@gmail.com>
2024-02-04 12:26:41 -0800

    subtracts the top element of the stack from the second top element of the stack

---
+++ sub.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+  *sub - sustration
+  *@stack: pointer to  stack
+  *@line_number: line_number
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *aux;
+	int sus, nodes;
+
+	aux = *stack;
+	for (nodes = 0; aux != NULL; nodes++)
+		aux = aux->next;
+	if (nodes < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		free_data();
+		exit(EXIT_FAILURE);
+	}
+	aux = *stack;
+	sus = aux->next->n - aux->n;
+	aux->next->n = sus;
+	*stack = aux->next;
+	free(aux);
+}
