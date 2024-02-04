82401ca6f53cdbf0809fd5f771d2999a2e4be5fd
netto-byte <marvellousmatthew@gmail.com>
2024-02-04 13:25:26 -0800

    Execute commands

--- cmd_execute.c
+++ cmd_execute.c
@@ -10,7 +10,7 @@ void get_command(void)
 		{"pop", pop}, {"swap", swap}, {"add", add},
 		{"nop", nop}, {"sub", sub}, {"div", _div},
 		{"mul", mul}, {"mod", mod}, {"pchar", pchar},
-		{"pstr", pstr}, {NULL, NULL}
+		{"pstr", pstr}, {"rotl", rotl}, {NULL, NULL}
 	};
 	int i;
 	char *cmd;
