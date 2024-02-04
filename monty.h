888434692d1180469c8a897e07f3d630afbede97
netto-byte <marvellousmatthew@gmail.com>
2024-02-04 14:46:00 -0800

    Header file

--- monty.h
+++ monty.h
@@ -73,6 +73,7 @@ void mod(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_mnumber);
+void rotr(stack_t **stack, unsigned int line_number);
 
 void free_data(void);
 void free_stack(stack_t **stack);
